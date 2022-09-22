176a99dfb98491a3c1d7a648dbb674a997cf352f
a-zhongg <amyzhong16@gmail.com>
2022-09-21 23:30:40 -0700

    fourth commit

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -5,11 +5,9 @@ import androidx.room.PrimaryKey
 import java.util.*
 import java.util.Date
 
-@Entity
 
-data class Crime {
-    data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
-                     var title: String ="",
-                     var date: Date = Date(),
-                     var isSolved: Boolean = false)
-}
+@Entity
+data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false)
